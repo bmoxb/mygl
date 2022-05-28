aa7a2a45bf7c245e8740a642ae5d85bff46cbf50
WiredSound <maxoblack@yahoo.com>
2022-05-28 22:35:22 +0100

    document root-level and rendering module functions and types

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,12 @@ pub mod vao;
 
 pub use error::Error;
 
+/**
+ * Clears the screen in the color specified.
+ *
+ * Each red/green/blue/alpha channel value should be between 0.0 and 1.0
+ * inclusive.
+ */
 pub fn clear(r: f32, g: f32, b: f32, a: f32) {
     unsafe {
         gl::ClearColor(r, g, b, a);
@@ -17,12 +23,23 @@ pub fn clear(r: f32, g: f32, b: f32, a: f32) {
     }
 }
 
+/**
+ * Enable wireframe drawing (draw everything as lines instead of filling in
+ * faces).
+ *
+ * Equivalent to: `glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)`
+ */
 pub fn enable_wireframe_rendering() {
     unsafe {
         gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
     }
 }
 
+/**
+ * Disable wireframe drawing (draw everything with faces filled in).
+ *
+ * Equivalent to: `glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)`
+ */
 pub fn disable_wireframe_rendering() {
     unsafe {
         gl::PolygonMode(gl::FRONT_AND_BACK, gl::FILL);
--- src/rendering.rs
+++ src/rendering.rs
@@ -1,3 +1,12 @@
+//! Module containing functions that facilitate rendering to the OpenGL context.
+//!
+//! All rendering functions assume nothing about the current OpenGL state in the
+//! sense that all shader programs, vertex array objects, etc. that passed into
+//! a rendering function are activated, bound, etc. in the function body as
+//! necessary - prior calls to [`VertexArrayObject::bind`],
+//! [`ShaderProgram::use_program`], and similar are not necessary prior to
+//! calling any rendering functions.
+
 use std::{convert, ptr};
 
 use gl::types::*;
@@ -6,6 +15,53 @@ use crate::shaders::ShaderProgram;
 use crate::textures::{Texture, TextureType};
 use crate::vao::VertexArrayObject;
 
+/**
+ * Draw a vertex array.
+ *
+ * Works by using the shader program, binding the vertex array object (VAO),
+ * and then calling the unsafe OpenGL function `glDrawArrays`.
+ *
+ * The passed VAO should have at least one vertex array object and at least one
+ * attribute pointer set.
+ *
+ * The `fist` and `count` parameters indicate the vertex to begin drawing from and
+ * the total number of vertices to draw respectively.
+ *
+ * # Examples
+ *
+ * ```
+ * let vert = VertexShader::from_file("example.vert").unwrap();
+ * let frag = FragmentShader::from_file("example.frag").unwrap();
+ * let prog = ShaderProgram::new(&vert, &frag).unwrap();
+ *
+ * let data: [f32; 9] = [
+ *     -0.5, -0.5, 0.0,
+ *      0.5, -0.5, 0.0,
+ *      0.0,  0.5, 0.0,
+ * ];
+ * let vbo = VertexBufferObject::new(&data, BufferUsageHint::Static);
+ *
+ *
+ * let vao = VertexArrayObjectBuilder::new()
+ *     .attribute(
+ *         &vbo,
+ *         VertexAttribute {
+ *             layout_index: 0,
+ *             component_count: 3,
+ *             component_type: VertexAttributeType::Float,
+ *             normalize: false,
+ *             stride: 3 * 4,
+ *             offset: 0,
+ *         },
+ *     )
+ *     .build();
+ *
+ * while window.is_open() { // render loop
+ *     rendering::draw_arrays(&prog, &vao, DrawMode::Triangles, 0, 3);
+ *     window.swap_buffers();
+ * }
+ * ```
+ */
 pub fn draw_arrays(
     prog: &ShaderProgram,
     vao: &VertexArrayObject,
@@ -29,6 +85,19 @@ pub fn draw_arrays(
     }
 }
 
+/**
+ * Draw a vertex array with some set of textures.
+ *
+ * Each texture is activated according to its index in the array passed (i.e.,
+ * the first texture is activated at `GL_TEXTURE0`, the second at `GL_TEXTURE1`,
+ * and so on).
+ *
+ * The passed VAO should have at least one vertex array object and at least one
+ * attribute pointer set.
+ *
+ * The `fist` and `count` parameters indicate the vertex to begin drawing from and
+ * the total number of vertices to draw respectively.
+ */
 pub fn draw_arrays_with_textures<const T: TextureType>(
     prog: &ShaderProgram,
     vao: &VertexArrayObject,
@@ -41,6 +110,15 @@ pub fn draw_arrays_with_textures<const T: TextureType>(
     draw_arrays(prog, vao, mode, first, count);
 }
 
+/**
+ * Draw a vertex array using indices.
+ *
+ * The passed vertex array object is expected to have an element buffer object
+ * set containing indices of the type `index_type`.
+ *
+ * The `count` parameter specifies the number of elements/indices to draw (not
+ * vertices).
+ */
 pub fn draw_elements(
     prog: &ShaderProgram,
     vao: &VertexArrayObject,
@@ -64,6 +142,19 @@ pub fn draw_elements(
     }
 }
 
+/**
+ * Draw a vertex array using indices and some set of textures.
+ *
+ * The passed vertex array object is expected to have an element buffer object
+ * set containing indices of the type `index_type`.
+ *
+ * Each texture is activated according to its index in the array passed (i.e.,
+ * the first texture is activated at `GL_TEXTURE0`, the second at `GL_TEXTURE1`,
+ * and so on).
+ *
+ * The `count` parameter specifies the number of elements/indices to draw (not
+ * vertices).
+ */
 pub fn draw_elements_with_textures<const T: TextureType>(
     prog: &ShaderProgram,
     vao: &VertexArrayObject,
@@ -76,6 +167,9 @@ pub fn draw_elements_with_textures<const T: TextureType>(
     draw_elements(prog, vao, index_type, mode, count);
 }
 
+/**
+ * Represents the type of indices contained within an element buffer object.
+ */
 #[derive(Copy, Clone, Debug)]
 pub enum IndexType {
     UnsignedByte,
@@ -93,6 +187,12 @@ impl convert::From<IndexType> for GLenum {
     }
 }
 
+/**
+ * Represents the draw mode to use. Each variant is mapped to an OpenGL enum of
+ * the same name (e.g., `DrawMap::Triangles` becomes `GL_TRIANGLES`) so consult
+ * the OpenGL documentation for an explanation of each of the different draw
+ * modes.
+ */
 #[derive(Copy, Clone, Debug, strum_macros::Display)]
 #[strum(serialize_all = "snake_case")]
 pub enum DrawMode {
